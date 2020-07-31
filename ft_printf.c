3cbab941cdb8cbc96d10b78223b6ee570f31190f
Tollett Lamonica <tlamonic@va-a3.kzn.21-school.ru>
2020-07-31 17:05:27 +0300

    cc

--- ft_printf.c
+++ ft_printf.c
@@ -1,370 +1,364 @@
-#include "libftprintf.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+# define CONV "sdx"
+int			g_cout;
+
+char		*ft_strchr(char *str, char c)
+{
+	while (*str)
+	{
+		if (*str == c)
+			break;
+		str++;
+	}
+	if (*str == c)
+		return (str);
+	return (0);
+}
+
+int			ft_isdigit(char c)
+{
+	return ((c >= '0' && c <= '9'));
+}
+
+int			ft_to_nbr(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1 <= s2)
+	{
+		i = i * 10 + *s1 - '0';
+		s1++;
+	}
+	return (i);
+}
+
+int			getwidth(char *s1, char *s2, va_list *vlist)
+{
+	char	*tmp;
+	int		width;
+
+	width = 0;
+	while (s2 > s1)
+	{
+		tmp = s2;
+		while (ft_isdigit(*s2))
+			s2--;
+		if (*s2 == '*' && *(s2 - 1) != '.')
+		{
+			width = va_arg(*vlist, int);
+			break;
+		}
+		else if (*s2 != '.' && s2 != tmp)
+		{
+			width = ft_to_nbr((char*)s2 + 1, (char*)tmp);
+			break;
+		}
+		s2--;
+	}
+	return (width);
+}
+
+int			getpres(char *s1, char *s2, va_list *vlist)
+{
+	int		pres;
+	char	*tmp;
+
+	pres = -1;
+	while (s2 > s1)
+	{
+		tmp = s2;
+		while (ft_isdigit(*s2))
+			s2--;
+		if (*s2 == '*' && *(s2 - 1) == '.')
+		{
+			pres = va_arg(*vlist, int);
+			break;
+		}
+		else if (*s2 == '.' && !ft_isdigit(*(s2 + 1)))
+		{
+			pres = 0;
+			break;
+		}
+		else if (*s2 == '.' && s2 != tmp)
+		{
+			pres = ft_to_nbr((char*)s2 + 1, tmp);
+			break;
+		}
+		s2--;
+	}
+	return (pres);
+}
+
+int			*getformat(char *s1, char *s2, va_list *vlist)
+{
+	int		*format;
+
+	format = malloc(sizeof(int) * 2);
+	format[0] = getwidth(s1, s2, vlist);
+	format[1] = getpres(s1, s2, vlist);
+	return (format);
+}
+
+int			ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int			prints(int *arr, int *format)
+{
+	char	*str;
+	int		size;
+	int		i;
+
+	if (!format)
+		return (0);
+	i = 0;
+	if (!arr)
+		str = "(null)";
+	else
+		str = (char*)arr;
+	size = format[1] > -1 ? format[1] : ft_strlen(str);
+	while (format[0]-- > size)
+		g_cout += write(1, " ", 1);
+	while (i < size)
+		g_cout += write(1, &str[i++], 1);
+	return (0);
+}
+
+void		ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void		ft_putnbr(int i)
+{
+	if (i == INT_MIN)
+	{
+		g_cout += write(1, "-2147483648", 11);
+		return ;
+	}
+	if (i < 0)
+		i *= -1;
+	if (i > 9)
+	{
+		ft_putnbr(i / 10);
+		ft_putchar(i % 10 + '0');
+	}
+	else
+		ft_putchar(i + '0');
+}
+
+int			getsize(int i)
+{
+	int size;
+
+	size = 0;
+	if (i == INT_MIN)
+		return (11);
+	if (i < 0)
+		i *= -1;
+	if (i == 0)
+		return (1);
+	while (i > 0)
+	{
+		size++;
+		i /= 10;
+	}
+	return (size);
+}
+
+char		getbasechar(int c)
+{
+	if (c >= 10 && c <= 15)
+		return (c + 87);
+	else if (c >= 0 && c <= 9)
+		return (c + '0');
+	else
+		return ('~');
+}
+
+void		reversstr(char *str, int size)
+{
+	int		i;
+	char	tmp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		tmp = str[i];
+		str[i] = str[size - 1];
+		str[size - 1] = tmp;
+		size--;
+		i++;
+	}
+}
+
+int			printd(int i, int *format)
+{
+	int		size;
+	int		tmp;
+
+	if (!format)
+		return (-1);
+	if (format[1] == 0 && i == 0)
+	{
+		while (format[0]-- > 0)
+			g_cout += write(1, " ", 1);
+		return (0);
+	}
+	size = format[1] > getsize(i) ? format[1] : getsize(i);
+	tmp = i < 0 ? size + 1 : size;
+	while (format[0]-- > tmp)
+		g_cout += write(1, " ", 1);
+	if (i < 0)
+		g_cout += write(1, "-", 1);
+	while (format[1]-- > getsize(i))
+		g_cout += write(1, "0", 1);
+	if (i == INT_MIN)
+		g_cout += write(1, "2147483648", 10);
+	else
+		ft_putnbr(i);
+	return (0);
+}
+
+char		*ulltoabase(int i)
+{
+	char	*res;
+	int		j;
+	int		size;
+
+	j = 0;
+	size = sizeof(unsigned long long) * 8 + 1;
+	res = malloc(sizeof(char) * size);
+	while (j < size)
+		res[j++] = '\0';
+	j = 0;
+	while (i > 16)
+	{
+		res[j++] = getbasechar(i % 16);
+		i /= 16;
+	}
+	res[j] = getbasechar(i);
+	reversstr(res, ft_strlen(res));
+	return (res);
+}
+
+int			printx(int i, int *format)
+{
+	char	*str;
+	int		size;
+	int		j;
+
+	if (!format)
+		return (-1);
+	if (i == 0 && format[1] == 0)
+	{
+		while (format[0]-- > 0)
+			g_cout += write(1, " ", 1);
+		return (0);
+	}
+	j = 0;
+	str = ulltoabase(i);
+	size = format[1] >= 0 ? format[1] : ft_strlen(str);
+	while (format[0]-- > size)
+		g_cout += write(1, " ", 1);
+	while (size-- > ft_strlen(str))
+		g_cout += write(1, "0", 1);
+	while (str[j])
+		g_cout += write(1, &str[j++], 1);
+	free(str);
+	return (0);
+}
+
+int			getparam(char c, int *format, va_list *vlist)
+{
+	int		errflag;
+
+	errflag = 0;
+	if (c == 's')
+		errflag = prints(va_arg(*vlist, int*), format);
+	else if (c == 'd')
+		errflag = printd(va_arg(*vlist, int), format);
+	else if (c == 'x')
+		errflag = printx(va_arg(*vlist, int), format);
+	return (errflag);
+}
+
+int			printparam(char *s1, char *s2, va_list *vlist)
+{
+	int		errflag;
+	int		*format;
+
+	if ((!s1 && !s2) || !*s2)
+		return (-1);
+	errflag = 0;
+	if (*s1 == '%' && *s2 == '%' && s2 - s1 == 1)
+		g_cout += write(1, "%", 1);
+	else if (*s1 == '%' && ft_strchr(CONV, *s2))
+	{
+		format = getformat(s1, s2, vlist);
+		if (getparam(*s2, format, vlist) == -1)
+			errflag = -1;
+		free(format);
+		format = NULL;
+	}
+	return (errflag);
+}
+
+int			parse_str(char *str, va_list *vlist)
+{
+	char	*tmp;
+
+	while (*str)
+	{
+		tmp = str;
+		if (*str == '%')
+		{
+			str++;
+			while (!ft_strchr(CONV, *str) && *str)
+				str++;
+			if (printparam(tmp, str, vlist) == -1)
+				return (-1);
+		}
+		else
+		{
+			while (*(str + 1) && *(str + 1) != '%')
+				str++;
+			g_cout += write(1, tmp, *str ? str - tmp + 1 : str - tmp);
+		}
+		str += *str ? 1 : 0;
+	}
+	return (0);
+}
+
+int			ft_printf(char *str, ...)
+{
+	va_list	vlist;
+	int		errflag;
+
+	if (!str)
+		return (-1);
+	g_cout = 0;
+	va_start(vlist, str);
+	errflag = parse_str(str, &vlist);
+	va_end(vlist);
+	if (errflag == -1)
+		return (-1);
+	return (g_cout);
+}
 #include <stdio.h>
-static char    *ft_strchr(char *str, char c)
-{
-    int i;
-
-    if (!str)
-        return (0);
-    i = -1;
-    while (str[++i])
-    {
-        if (str[i] == c)
-            break;
-    }
-    if (str[i] == c)
-        return (&str[i]);
-    return (0);
-}
-
-static int     ft_to_nbr(char *start, char *end)
-{
-    int     res;
-
-    res = 0;
-    while (end >= start)
-    {
-        res = res * 10 + (*start - '0');
-        start++;
-    }
-    return (res);
-}
-
-static int     ft_isdigit(char c)
-{
-    return ((c >= '0' && c <= '9'));
-}
-
-static int     getsize(int i)
-{
-    int size;
-
-    size = 0;
-    if (i == INT_MIN)
-        return (11);
-    if (i == 0)
-        return (1);
-    if (i < 0)
-    {
-        size++;
-        i *= -1;
-    }
-    while (i)
-    {
-        size++;
-        i /= 10;
-    }
-    return (size);
-}
-
-static size_t  ft_strlen(char *str)
-{
-    size_t size;
-
-    size = 0;
-    while (str[size])
-        size++;
-    return (size);
-}
-
-static void     ft_putchar(char c)
-{
-    g_cout += write(1, &c, 1);
-}
-
-static void    ft_putnbr(int i)
-{
-    if (i == INT_MIN)
-    {
-        g_cout += write(1, "-2147483648", 11);
-        return ;
-    }
-    if (i < 0)
-    {
-        i *= -1;
-        g_cout += write(1, "-", 1);
-    }
-    if (i > 9)
-    {
-        ft_putnbr(i / 10);
-        ft_putchar((i % 10) + '0');
-    }
-    else
-        ft_putchar(i + '0');
-}
-
-static char     getbasechar(int c)
-{
-    if (c > 9 && c < 16)
-        return (c + 87);
-    else if (c >= 0 && c < 10)
-        return (c + '0');
-    else
-        return ('~');
-}
-
-static void     reversstr(char *str, size_t size)
-{
-    size_t i;
-    char    tmp;
-
-    i = 0;
-    while (i < size - 1)
-    {
-        tmp = str[i];
-        str[i] = str[size - 1];
-        str[size - 1] = tmp;
-        size--;
-        i++;
-    }
-}
-
-static char     *ulltoabase(int i, int form)
-{
-    char *str;
-    size_t size;
-    size_t tmp;
-
-    tmp = 0;
-    size = sizeof(long long) * 8 + 1;
-    if (!(str = (char*)malloc(sizeof(char) * size)))
-        return 0;
-    while (tmp < size)
-        str[tmp++] = '\0';
-    tmp = 0;
-    while (i >= form)
-    {
-        str[tmp++] = getbasechar(i % form);
-        i /= form;
-    }
-    str[tmp] = getbasechar(i);
-    reversstr(str, ft_strlen(str));
-    return (str);
-}
-
-void            strtoupper(char *str)
-{
-    while (*str)
-    {
-        if (*str >= 'a' && *str <= 'z')
-            *str -=32;
-        str++;
-    }
-}
-
-static int     printx(unsigned int i, int *format, char c)
-{
-    char    *res;
-    int     size;
-    int     j;
-
-    if (!format)
-        return (-1);
-    j = -1;
-    if (i == 0 && format[1] == 0)
-    {
-        while (format[0]--)
-            g_cout += write(1, " ", 1);
-        return (0);
-    }
-    if (!(res = ulltoabase(i, 16)))
-        return (-1);
-    if (c == 'X')
-        strtoupper(res);
-    size = format[1] >= 0 ? format[1] : ft_strlen(res);
-    while (format[0]-- > size)
-        g_cout += write(1, " ", 1);
-    while (size-- > (int)ft_strlen(res))
-        g_cout += write(1, "0", 1);
-    while (++j < (int)ft_strlen(res))
-        ft_putchar(res[j]);
-    free(res);
-    return (0);
-}
-
-static int     prints(int *arr, int *format)
-{
-    char    *str;
-    int     size;
-    int     i;
-
-    if (!format)
-        return (-1);
-    str = (char*)arr;
-    i = -1;
-    size = format[1] >= 0 ? format[1] : ft_strlen(str);
-    while (format[0]-- > size)
-        g_cout += write(1, " ", 1);
-    while (++i < size)
-        ft_putchar(str[i]);
-    return (0);
-}
-
-static int     printd(int i, int *format)
-{
-    int size;
-
-    if (!format)
-        return (-1);
-    size = getsize(i);
-    while (format[0]-- > format[1])
-        g_cout += write(1, " ", 1);
-    while (format[1]-- > size)
-        g_cout += write(1, "0", 1);
-    ft_putnbr(i);
-    return (0);
-}
-
-static int     getwidth(char *str, char *str2, va_list *vlist)
-{
-    char    *tmp;
-    int     width;
-
-    width = 0;
-    while (str2 > str)
-    {
-        tmp = str2;
-        while (ft_isdigit(*str2))
-            str2--;
-        if (*str2 == '*' && *(str2 - 1) != '-')
-        {
-            width = va_arg(*vlist, int);
-            break;
-        }
-        else if (*str2 != '.' && str2 != tmp)
-        {
-            width = ft_to_nbr((char*)str2 + 1, (char*)tmp);
-            break;
-        }
-        str2--;
-    }
-    return (width);
-}
-
-static int     getpres(char *s1, char *s2, va_list *vlist)
-{
-    int     pres;
-    char    *tmp;
-
-    pres = -1;
-    while (s2 > s1)
-    {
-        tmp = s2;
-        while (ft_isdigit(*s2))
-            s2--;
-        if (*s2 == '.' && !ft_isdigit(*(s2 + 1)))
-            return (0);
-        if (*s2 == '*' && *(s2 - 1) == '.')
-        {
-            pres = va_arg(*vlist, int);
-            break;
-        }
-        else if (*s2 == '.' && s2 != tmp)
-        {
-            pres = ft_to_nbr((char*)s2 + 1, (char*)tmp);
-            break;
-        }
-        s2--;
-    }
-    return (pres);
-}
-
-static int     *getformat(char *s1, char *s2, va_list *vlist)
-{
-    int *format;
-
-    if (!s2 && !s2)
-        return (0);
-    if (!(format = malloc(sizeof(int) * 2)))
-        return (0);
-    format[0] = getwidth(s1, s2, vlist);
-    format[1] = getpres(s1, s2, vlist);
-    return (format);
-}
-
-static int     getparam(char *str, int *format, va_list *vlist)
-{
-    int errflag;
-
-    errflag = 0;
-    if (*str == 'd' || *str == 'i')
-        errflag = printd(va_arg(*vlist, int), format);
-        else if (*str == 'x')
-        errflag = printx(va_arg(*vlist, int), format, *str);
-    else if (*str == 's')
-        errflag = prints(va_arg(*vlist, int*), format);
-    return (errflag);
-}
-
-static int             print_param(char *str, char *tmp, va_list *vlist)
-{
-    int errflag;
-    int *format;
-
-    if (!str && !tmp)
-        return (-1);
-    if (!*tmp)
-        return (-1);
-    errflag = 0;
-    if (*tmp == '%' && *str == '%' && tmp - str == 1)
-        g_cout += write(1, "%", 1);
-    else if (*str == '%' && ft_strchr(CONV, *tmp))
-    {
-        format = getformat(str, tmp, vlist);
-        if (getparam(tmp, format, vlist) == -1)
-            errflag = -1;
-        free(format);
-        format = NULL;
-    }
-    return (errflag);
-}
-
-static int     parse_str(char *str, va_list *vlist)
-{
-    char    *tmp;
-
-    while (*str)
-    {
-        tmp = str;
-        if (*str == '%')
-        {
-            str++;
-            while (!ft_strchr(CONV, *str) && *str)
-                str++;
-            if (print_param(tmp, str, vlist) == -1)
-                return (-1);
-        }
-        else
-        {
-            while (*(str + 1) && *(str + 1) != '%')
-                str++;
-            g_cout += write(1, tmp, *str ? str - tmp + 1: str - tmp);
-        }
-        str += *str ? 1 : 0;
-    }
-    return 0;
-}
-
-int     ft_printf(char *str, ...)
-{
-    int         errflag;
-    va_list     argptr;
-
-    if (!str)
-        return (-1);
-    errflag = 0;
-    g_cout = 0;
-    va_start(argptr, str);
-    errflag = parse_str(str, &argptr);
-    va_end(argptr);
-    if (errflag == -1)
-        return (-1);
-    return (g_cout);
-}
-
 int main()
 {
-    ft_printf("count of symbols is %d\n\n\n\n\n", ft_printf("Hello World! %10.6s\n\n\n", "HelloWorld"));
-    printf("count of symbols is %d\n\n\n\n\n", printf("Hello World! %10.6s\n\n\n", "HelloWorld"));
-    return 0;
+	ft_printf("%10.3s", NULL);
+	printf("%%\n");
+	printf("%10.3s", NULL); 
 }
